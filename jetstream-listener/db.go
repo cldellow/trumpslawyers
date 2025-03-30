b25d318dcb4bc3a43bb5b99adea87f505f080201
Colin Dellow <cldellow@gmail.com>
2025-03-30 18:14:57 -0400

    rename progress -> cursor
    To better match the jetstream API

--- jetstream-listener/db.go
+++ jetstream-listener/db.go
@@ -13,7 +13,7 @@ func initDB(dbPath string) (*sql.DB, error) {
 	// A table to track the highwatermark of our Jetstream listener,
 	// so we can resume if interrupted.
 	createTableSQL := `
-	CREATE TABLE IF NOT EXISTS progress (
+	CREATE TABLE IF NOT EXISTS cursor (
 		time_us INTEGER
 	);`
 	_, err = db.Exec(createTableSQL)
@@ -21,17 +21,18 @@ func initDB(dbPath string) (*sql.DB, error) {
 		return db, err
 	}
 
-	_, err = db.Exec(`INSERT INTO progress(time_us) SELECT 0 WHERE NOT EXISTS(SELECT * FROM progress)`)
+	_, err = db.Exec(`INSERT INTO cursor(time_us) SELECT 0 WHERE NOT EXISTS(SELECT * FROM cursor)`)
 	if err != nil {
 		return db, err
 	}
 
 	createTableSQL = `
-	CREATE TABLE IF NOT EXISTS skeets (
+	CREATE TABLE IF NOT EXISTS post_mentions (
 		did TEXT NOT NULL,
 		rkey TEXT NOT NULL,
 		time_us INTEGER NOT NULL,
 		json TEXT NOT NULL,
+		processed BOOLEAN NOT NULL DEFAULT FALSE,
 		PRIMARY KEY(did, rkey)
 	);`
 	_, err = db.Exec(createTableSQL)
@@ -44,7 +45,7 @@ func initDB(dbPath string) (*sql.DB, error) {
 
 func getCursor(db *sql.DB) (int64, error) {
 	var count int64
-	err := db.QueryRow(`SELECT time_us FROM progress`).Scan(&count)
+	err := db.QueryRow(`SELECT time_us FROM cursor`).Scan(&count)
 	if err != nil {
 		return 0, err
 	}
@@ -53,7 +54,7 @@ func getCursor(db *sql.DB) (int64, error) {
 }
 
 func setCursor(db *sql.DB, time_us int64) (error) {
-	_, err := db.Exec(`UPDATE progress SET time_us = ?`, time_us)
+	_, err := db.Exec(`UPDATE cursor SET time_us = ?`, time_us)
 	return err
 }
 
